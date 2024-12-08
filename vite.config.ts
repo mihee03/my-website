2759f79df1f5bdda91cdf06adb699746e639cdcb
mihee03 <rlaalgml0312@naver.com>
2024-12-08 15:07:01 +0900

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      // 프록시 서버 url 정의
+    },
+  },
+});
